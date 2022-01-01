9da2d76163a3246e900c2df97a03e87c164ae174
yosh-0827 <73326181+yosh-0827@users.noreply.github.com>
2022-01-02 01:00:08 +0900

    initial commit 入力内容を入力後、入力欄を空にする挙動が動かない

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import "./styles.css";
 
 const onClickAdd = () => {
-  alert();
-}
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  alert(inputText);
+};
 
 document
-.getElementById("add-button")
-.addEventListener("click", () => onClickAdd)
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd);
